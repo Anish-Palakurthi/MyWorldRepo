f6979a0a91216ce44ce3c482533beb503fa56c26
Anish Palakurthi <anishpalakurthi@Anishs-MacBook-Pro.local>
2022-02-15 12:02:29 -0600

    renders through register as before draws error when verifying code: 2022-02-15 12:01:15.554830-0600 MyWorld[21718:831765] [boringssl] boringssl_metrics_log_metric_block_invoke(151) Failed to log metrics
    freezes after attempting to pass through credit card initializer -- won't progress further. need to work with the NavView to determine why.

--- MyWorld/MyWorldApp.swift
+++ MyWorld/MyWorldApp.swift
@@ -11,6 +11,7 @@ import Firebase
 
 @main
 struct MyWorldApp: App {
+    @UIApplicationDelegateAdaptor(Appdelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -23,5 +24,19 @@ struct MyWorldApp: App {
     }
 }
 
+class Appdelegate : NSObject, UIApplicationDelegate{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+                     launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool{
+        
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+    }
+}
 
 
+
+    
