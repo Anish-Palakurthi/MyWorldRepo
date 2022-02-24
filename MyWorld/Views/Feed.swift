7a5839137e0c126f31f68833289ac7ea73fa2185
Anish Palakurthi <anishpalakurthi@Anishs-MacBook-Pro.local>
2022-02-24 12:08:45 -0600

    tried incorporating new post/feed models

--- MyWorld/Views/Feed.swift
+++ MyWorld/Views/Feed.swift
@@ -9,11 +9,45 @@ import SwiftUI
 
 struct Feed: View {
     var body: some View {
+        
+        NavigationView {
+                    GeometryReader { geometry in
+                        List {
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack {
+                                        Text("Story 1")
+                                        Text("Story 2")
+                                        Text("Story 3")
+                                        Text("Story 4")
+                                        Text("Story 5")
+                                    }
+                                }
+                                VStack(alignment: .leading, spacing: 8) {
+                                   Spacer()
+                                    Image("postImage")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: geometry.size.width, height: 250)
+                                        .clipped()
+                                    Text("text here...")
+                                        .lineLimit(nil)
+                                        .font(.system(size: 15))
+                                        .padding(.leading, 16).padding(.trailing, 16).padding(.bottom, 16)
+                                }.listRowInsets(EdgeInsets())
+                            }.navigationBarTitle(Text("MyWorld"), displayMode: .inline)
+                    }
+                        }
+                }
+            }
+    
+        
+        /*
         ScrollView {
             VStack(spacing: 20) {
                 ForEach(posts){post in
                     VStack{
                         Image(post.imageName)
+                            .frame(width: 500, height: 500, alignment: .center)
                         Text(post.description)
                     }
                     
@@ -23,9 +57,11 @@ struct Feed: View {
                
             }
         }
-    }
-}
+        */
 
+/*
+
+ */
 struct Feed_Previews: PreviewProvider {
     static var previews: some View {
         Feed()
--- MyWorld/Views/Post.swift
+++ MyWorld/Views/Post.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct Post: View {
+    let post: PostModel
+    let screenWidth: CGFloat
     var body: some View {
-        Image("farming").padding(.all)
+        VStack(alignment: .leading, spacing: 8) {
+            HStack(spacing: 8) {
+                Image(post.imageName)
+                    .resizable()
+                    .clipShape(Circle())
+                    .frame(width: 50, height: 50)
+                Text(post.category).font(.headline)
+            }.padding(EdgeInsets(top: 8, leading: 16, bottom: 0, trailing: 0))
+            Image(post.imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(width: screenWidth, height: 250)
+                .clipped()
+            Text(post.description)
+                .lineLimit(nil)
+                .font(.system(size: 15))
+                .padding(.leading, 16).padding(.trailing, 16).padding(.bottom, 16)
+        }.listRowInsets(EdgeInsets())
     }
 }
 
+
 struct Post_Previews: PreviewProvider {
     static var previews: some View {
-        Post()
+        Post(post: PostModel, screenWidth: UIScreen.main.bounds.size.width)
     }
 }
